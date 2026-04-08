47ac5478b7c35c921f3c71a64af628cc180db511
Don Girman <dgirman@girman.com>
2026-04-08 10:14:50 -0700

    updated py_funcs.py with test fuction:
    def py_test(a,b):update for test function and app

--- pythonProject3/py_functions.py
+++ pythonProject3/py_functions.py
@@ -3,13 +3,26 @@
 
 from pyxll import xl_func
 
+
 @xl_func
-def py_test_func(a,b):
-	return f"a = {a}; b = {b}"
+def py_test_func_01(a, b, c):
+	return f"a = {a}; b = {b} ; c = {c}"
+
+
+def py_test_func_02(a, b, c):
+	return a + b + c
+
+@xl_func
+def py_test_func(a,b, c):
+	return [str(type(a)), str(type(b)), str(type(c))]
 
 
 def test():
+	"""
+	:return:
+	"""
 	rtn = py_test_func(a=1,b=2)
 	print(rtn)
+
 if __name__ == "__main__":
     test()
